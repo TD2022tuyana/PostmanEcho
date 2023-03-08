13e6217bc1f69e7f8cd1830922c53e2655378bc2
TD2022tuyana <112084157+TD2022tuyana@users.noreply.github.com>
2023-03-08 21:49:33 +0300

    add postman test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,15 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 public class PostmanEchoTest {
-    // Given - When - Then
-// Предусловия
-    given()
-  .baseUri("https://postman-echo.com")
-  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-.when()
-  .post("/post")
-// Проверки
-.then()
-  .statusCode(200)
-  .body(/* --> ваша проверка здесь <-- */)
-            ;
+  @Test
+    public void testPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
 }
